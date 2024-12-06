9a692f077d487cd837ef32c8638f988c6ad7cf07
Juliano Candido <julianosgs@yahoo.com.br>
2024-12-06 08:23:04 -0300

    Implementação da diretiva ngModel nos inputs

--- src/app/components/input-senha/input-senha.component.ts
+++ src/app/components/input-senha/input-senha.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-input-senha',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './input-senha.component.html',
   styleUrl: './input-senha.component.css'
 })
 export class InputSenhaComponent {
-
+   name:string ="";
 }
